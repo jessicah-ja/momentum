e9f18d14dc58bc0e0e2bb3c0dc063b42b38b423f
jessicah-ja <53472264+jessicah-ja@users.noreply.github.com>
2019-11-27 22:53:09 +0900

    clock&greeting part commit

---
+++ clock.js
@@ -0,0 +1,23 @@
+const clockContainer = document.querySelector('.js-clock'),
+      clockTitle = clockContainer.querySelector('h1');
+
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${ hours < 10 ? `0${hours}`: hours }:${ 
+        minutes < 10 ? `0${minutes}`: minutes }:${ seconds < 10 ? `0${seconds}`: seconds }`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000); 
+}
+
+init();
+
+
+//setIntervel()
+//첫번쨰 인자로 실행할 함수를 받고 두번쨰 인자로는 그 함수를 실행하고 싶은 시간 간격
